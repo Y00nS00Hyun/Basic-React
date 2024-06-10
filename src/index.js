87a27f36212ba8361d7e1786f44b2d9e82fc8bc0
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-11 04:44:12 +0900

    오류

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>안녕 리액트!</h1>);
-
-//구 버전: ReactDOM.render(<h1>수현 바보!</h1>, document.getElementById("root"));
-
-const root2 = ReactDOM.createRoot(document.getElementById("root2"));
-root2.render(<p className="soohyun">뭘봐 리액트!</p>);
+root.render(<p>귀요미</p> <p>수혀니</p>, document.getElementById("root"));
