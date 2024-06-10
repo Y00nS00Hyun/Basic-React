cbe9f3928cb17f60576f2f6e66e06e4a1a124336
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-11 05:12:06 +0900

    글자 합치기

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom/client";
 
-const butler = "butler";
+const butler = "집사";
+const name = "세미";
+const butlerName = butler + name;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <p>나만의 {butler.toUpperCase()} 구하기</p> //대문자 변환 메소드
+    <p>나만의 {butler + name} 구하기</p>
+    <p>나만의 {butlerName} 구하기</p>
   </>,
   document.getElementById("root")
 );
