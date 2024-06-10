de318c7d0065c92b9db311ed1be8527b7718d0ac
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-11 04:44:44 +0900

    <div>로 묶어서 오류 해결

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<p>귀요미</p> <p>수혀니</p>, document.getElementById("root"));
+root.render(
+  <div>
+    <p>귀요미</p> <p>수혀니</p>
+  </div>,
+  document.getElementById("root")
+);
