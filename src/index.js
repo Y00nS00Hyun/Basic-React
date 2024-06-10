844ac7d2a2f378efa3ae9e05c2366b46ebe0eb62
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-11 05:17:52 +0900

    이미지 첨부하기

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 const butler = "집사";
 const name = "세미";
 const butlerName = butler + name;
+const imageUrl =
+  "https://lh3.googleusercontent.com/c1uC2qvKKdCdCcO-SWZqukyNF6GLHqyxYCrQSdIhcBtlVxR4KYgU37UWtrEyi5P10meBRL5zEY0NoIcqmA";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <p>나만의 {butler + name} 구하기</p>
     <p>나만의 {butlerName} 구하기</p>
+    <img src={imageUrl} alt="제품사진" />
   </>,
   document.getElementById("root")
 );
