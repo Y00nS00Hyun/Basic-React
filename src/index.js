2533a11f673006506f6ce139837ccd6645dd9425
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-11 05:09:32 +0900

    대문자 변환 메소드

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import ReactDOM from "react-dom/client";
 
-const butler = "집사";
+const butler = "butler";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <p>나만의 {butler} 구하기</p>
+    <p>나만의 {butler.toUpperCase()} 구하기</p> //대문자 변환 메소드
   </>,
   document.getElementById("root")
 );
