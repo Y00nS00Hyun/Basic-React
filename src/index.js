eefdaeffdcc55b6443dcaa92fc9e51f90678f0a5
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-11 04:35:12 +0900

    h1 -> p

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ root.render(<h1>안녕 리액트!</h1>);
 //구 버전: ReactDOM.render(<h1>수현 바보!</h1>, document.getElementById("root"));
 
 const root2 = ReactDOM.createRoot(document.getElementById("root2"));
-root2.render(<h1>뭘봐 리액트!</h1>);
+root2.render(<p id="soohyun">뭘봐 리액트!</p>);
