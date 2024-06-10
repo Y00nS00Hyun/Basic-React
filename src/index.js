937ce6e48212a0a08220b620878eef4904239677
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-11 04:29:37 +0900

    구 버전과 비교

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<h1>안녕 리액트!</h1>);
+
+//구 버전: ReactDOM.render(<h1>수현 바보!</h1>, document.getElementById("root"));
