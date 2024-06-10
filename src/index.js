f5f7a1fcd4d07884d2e4b39ac010cad626808765
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-11 04:46:32 +0900

    Fragment 축약형

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import { Fragment } from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Fragment>
+  <>
     <p>귀요미</p> <p>수혀니</p>
-  </Fragment>,
+  </>,
   document.getElementById("root")
 );
