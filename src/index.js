eb9472c91a5d98e045bd8c84a42f46392c0d0f3b
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-11 04:31:27 +0900

    html 과 js 왔다갔다 하기

--- src/index.js
+++ src/index.js
@@ -4,3 +4,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<h1>안녕 리액트!</h1>);
 
 //구 버전: ReactDOM.render(<h1>수현 바보!</h1>, document.getElementById("root"));
+
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+root2.render(<h1>뭘봐 리액트!</h1>);
