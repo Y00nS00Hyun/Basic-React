1a597087d94ad006085aaafd260748dfa99486b9
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-11 04:59:18 +0900

    변수 선언

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import ReactDOM from "react-dom/client";
 
+const butler = "집사";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <p>귀요미</p> <p>수혀니</p>
+    <p>나만의 {butler} 구하기</p>
   </>,
   document.getElementById("root")
 );
