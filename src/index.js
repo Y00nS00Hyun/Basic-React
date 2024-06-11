1f4a8f233abf038f3d5e30f00484537b316bc7bf
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-11 15:28:07 +0900

    컴포넌트

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
 import ReactDOM from "react-dom/client";
 
-const butler = "집사";
-const name = "세미";
-const butlerName = butler + name;
-const imageUrl =
-  "https://lh3.googleusercontent.com/c1uC2qvKKdCdCcO-SWZqukyNF6GLHqyxYCrQSdIhcBtlVxR4KYgU37UWtrEyi5P10meBRL5zEY0NoIcqmA";
-function handleClick() {
-  alert("어서오세요");
-}
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <>
-    <p>나만의 {butlerName} 구하기</p>
-    <img src={imageUrl} alt="제품사진" />
-    <button onClick={handleClick}>모집 중</button>
-  </>,
-  document.getElementById("root")
-);
+const element = <h1>안녕 수현아</h1>;
+
+root.render(<>{element}</>, document.getElementById("root"));
