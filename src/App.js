a44b8bc87dcefa309434cf0b9ef4f72744c8e33a
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-13 23:43:09 +0900

    children 사용 후

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function Apple() {
   return (
     <div>
       <div>
-        <Button text={"던지기"} />
-        <Button text={"처음부터"} />
+        <Button>던지기</Button>
+        <Button>처음부터</Button>
       </div>
       <Dice color="red" num={2} />
     </div>
