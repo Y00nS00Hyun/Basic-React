f09fb0828abd8d436be9d027fc120a6a0f422d3d
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-13 23:44:23 +0900

    우냠?

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function Apple() {
         <Button>던지기</Button>
         <Button>처음부터</Button>
       </div>
-      <Dice color="red" num={2} />
+      <Dice color="red" num={6} />
     </div>
   );
 }
