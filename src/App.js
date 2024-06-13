6a5b9ad13507e4c86e0af902b5da106b23728c60
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-14 04:13:30 +0900

    참조형 state

--- src/App.js
+++ src/App.js
@@ -9,15 +9,22 @@ function random(n) {
 
 function App() {
   const [num, setNum] = useState(1); // [state값, 세터 함수], 1로 초기화
+  const [sum, setSum] = useState(0); //총점
+  const [gameHistory, setGameHistory] = useState([]); //결과 기록, 배열 넣기
 
   const handleRollClick = () => {
     const nextNum = random(6);
     setNum(nextNum);
+    setSum(sum + nextNum); //총점
+    gameHistory.push(nextNum); //결과 기록
+    setGameHistory(gameHistory);
   };
 
   // 처음부터 버튼 누르면 1로 바뀜
   const handleClearClick = () => {
     setNum(1);
+    setSum(0);
+    setGameHistory([]);
   };
 
   return (
@@ -26,7 +33,14 @@ function App() {
         <Button onClick={handleRollClick}>던지기</Button>
         <Button onClick={handleClearClick}>처음부터</Button>
       </div>
-      <Dice color="red" num={num} />
+      <div>
+        <h2>나</h2>
+        <Dice color="red" num={num} />
+        <h2>총점</h2>
+        <p>{sum}</p>
+        <h2>기록</h2>
+        <p>{gameHistory.join(",")}</p>
+      </div>
     </div>
   );
 }
