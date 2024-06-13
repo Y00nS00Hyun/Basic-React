c5e9eab011ee0705fe1680397a7872cb0a9b5597
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-13 18:29:37 +0900

    설명 보완

--- src/Dice.js
+++ src/Dice.js
@@ -25,10 +25,11 @@ const DICE_IMAGES = {
   red: [diceRed01, diceRed02, diceRed03, diceRed04, diceRed05, diceRed06],
 };
 
-// 기본값을 설정하려면 함수 파라미터에서 직접 설정해야함
+// 기본값(입력 값이 없을 때 나타낼 값)을 설정하려면 함수 파라미터에서 직접 설정해야함
 function Dice({ color = "blue", num = 1 }) {
   // 중괄호
   const src = DICE_IMAGES[color][num - 1]; // DICE_IMAGES 객체에서 color와 num을 사용해 주사위 이미지를 찾음
+  //1.blue, red 배열 중 하나로 들어가서 2.arr[i]=num-1번째꺼를 가져옴
   const alt = `${color} ${num}`; // 이미지의 대체 텍스트를 색상과 번호로 설정
   return <img src={src} alt={alt} />; // 주사위 이미지를 렌더링
 }
