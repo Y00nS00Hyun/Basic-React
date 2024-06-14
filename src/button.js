9c44deeb91b52b7c81ed87640216431b05d506bf
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-14 18:22:18 +0900

    스타일 적용하기

--- src/button.js
+++ src/button.js
@@ -1,7 +1,22 @@
 // children: 바뀌지 않는 button 내용으로 사용됨, onclick으로는 안씀
 
+const style = {
+  padding: "14px 27px",
+  border: "solid 1px #7090ff",
+  outline: "none",
+  color: "#7090ff",
+  cursor: "pointer",
+  backgroundColor: "rgba(0, 89, 255, 0.2)",
+  borderRadius: "30px",
+  fontSize: "17px",
+};
+
 function Button({ children, onClick }) {
-  return <button onClick={onClick}>{children}</button>;
+  return (
+    <button style={style} onClick={onClick}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
