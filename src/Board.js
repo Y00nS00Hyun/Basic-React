2d0eeb4657720eb115fa713390b18badf24bf9ab
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-14 15:32:10 +0900

    코드 정리하기

--- src/Board.js
+++ src/Board.js
@@ -2,7 +2,9 @@ import Dice from "./Dice";
 
 // 랜덤
 
-function Board({ name, color, num, sum, gameHistory }) {
+function Board({ name, color, gameHistory }) {
+  const num = gameHistory[gameHistory.length - 1] || 1; //마지막 배열의 요소 갖고오기, 비어있으면 기본값인 1
+  const sum = gameHistory.reduce((a, b) => a + b, 0); //a 누적값, b 현재값, 0 초기값 / reduce: 배열을 순회하면서 누적함
   return (
     <div>
       <h2>{name}</h2>
