2bae5b718a973c82db1a86f1aec4af84b6ecf8d2
Y00nS00Hyun <yoonsooh1114@gmail.com>
2024-06-14 18:30:17 +0900

    스타일 적용 2

--- src/button.js
+++ src/button.js
@@ -1,17 +1,29 @@
 // children: 바뀌지 않는 button 내용으로 사용됨, onclick으로는 안씀
 
-const style = {
+const baseButtonStyle = {
   padding: "14px 27px",
-  border: "solid 1px #7090ff",
-  outline: "none",
-  color: "#7090ff",
-  cursor: "pointer",
-  backgroundColor: "rgba(0, 89, 255, 0.2)",
   borderRadius: "30px",
+  outline: "none",
+  cursor: "pointer",
   fontSize: "17px",
 };
 
-function Button({ children, onClick }) {
+const blueButtonStyle = {
+  ...baseButtonStyle,
+  backgroundColor: "rgba(0, 89, 255, 0.2)",
+  border: "solid 1px #7090ff",
+  color: "#7090ff",
+};
+
+const redButtonStyle = {
+  ...baseButtonStyle,
+  backgroundColor: "rgba(255, 78, 78, 0.2)",
+  border: "solid 1px #ff4664",
+  color: "#ff4664",
+};
+
+function Button({ children, onClick, color }) {
+  const style = color === "red" ? redButtonStyle : blueButtonStyle;
   return (
     <button style={style} onClick={onClick}>
       {children}
